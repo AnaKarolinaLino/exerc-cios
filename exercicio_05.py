8f4c38ce12923c34f662eab1de231d1b2d7005cc
karol-white <0001129651459sp@al.educacao.sp.gov.br>
2025-05-06 12:07:21 -0300

    adicionamos as notas de tal aluno no banco de dados

--- exercicio_05.py
+++ exercicio_05.py
@@ -1,13 +1,15 @@
-def cadastrar_aluno(nome, email, serie):
+def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
     
     alunos = []
 
     aluno =  {
         "nome": nome,
         "email": email,
-        "serie": serie
+        "serie": serie,
+        "nota": [nota01, nota02, nota03]
     }
     alunos.append(aluno)
     return alunos
 
-print(cadastrar_aluno("Ana Karolina", "00001129651459sp@al.educacao.sp.gov.br", "2TB"))
+print(cadastrar_aluno("Ana Karolina", "00001129651459sp@al.educacao.sp.gov.br", "2TB", 7, 9, 10))
+
