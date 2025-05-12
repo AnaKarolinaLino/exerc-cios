4476f3628cab45e076a67868051e66dcc9fc6e67
karol-white <0001129651459sp@al.educacao.sp.gov.br>
2025-05-12 14:18:28 -0300

    criamos a media dentro da funçao e configuramos as notas ao valor 0

--- exercicio_04.py
+++ exercicio_04.py
@@ -6,6 +6,3 @@ def somar_numeros(lista):
     media = total / len(lista)
     return media
 
-numeros = [2, 44, 74, 34, 55, 81]
-
-print(somar_numeros(numeros))
--- exercicio_05.py
+++ exercicio_05.py
@@ -1,15 +1,21 @@
-def cadastrar_aluno(nome, email, serie, nota01, nota02, nota03):
+from exercicio_04 import somar_numeros
+
+def cadastrar_aluno(nome, email, serie, nota01=0, nota02=0, nota03=0):
     
     alunos = []
+
+    notas =  [nota01, nota02, nota03]
 
     aluno =  {
         "nome": nome,
         "email": email,
         "serie": serie,
-        "nota": [nota01, nota02, nota03]
+        "nota": notas,
+        "media": somar_numeros(notas)
     }
     alunos.append(aluno)
+    
     return alunos
 
-print(cadastrar_aluno("Ana Karolina", "00001129651459sp@al.educacao.sp.gov.br", "2TB", 7, 9, 10))
+print(cadastrar_aluno("Ana Karolina", "00001129651459sp@al.educacao.sp.gov.br", "2TB", 10, 2, 9))
 
